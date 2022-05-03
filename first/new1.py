215f10df47f3ffcba6fc15e7742c0c8bdec80131
jaisai <jaisai8507@gmail.com>
2022-05-03 17:01:49 +0530

    new file

---
+++ first/new1.py
@@ -0,0 +1,11 @@
+a=int(input())
+for i in range(a):
+    z=int(input())
+    l=list(map(int,input().split()))
+    c=0
+    for j in range(1,z):
+        if(l[j-1]!=l[j]):
+            if((j-1)==0):
+                c=c-1
+            c=c+2
+    print(c)
